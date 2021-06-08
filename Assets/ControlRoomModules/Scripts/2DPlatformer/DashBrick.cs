136c83876c7d59ae30f548f327702a28d2761774
조을연 <carpinus0@gmail.com>
2021-06-08 22:52:02 +0900

    dash brick fix

--- Assets/ControlRoomModules/Scripts/2DPlatformer/DashBrick.cs
+++ Assets/ControlRoomModules/Scripts/2DPlatformer/DashBrick.cs
@@ -10,7 +10,10 @@ namespace ControlRoom
         public float DashDistance = 3f;
         public float DashForce = 40f;
         public bool ResetForcesOnExit = false;
-
+        public int SuccessiveDashAmount = 1;
+        public int SuccessiveDashesLeft = 1;
+        public float DashCooldown = 1f;
+        public bool LimitedDashes = false;
 
 
         private bool shouldKeepDashing = false;
@@ -21,25 +24,104 @@ namespace ControlRoom
         private IEnumerator dashCoroutine;
         private bool dashEndedNaturally = true;
 
+        private float startTime = 0f;
+        private float cooldownTimeStamp = 0f;
+        private float lastDashAt = 0f;
+
         protected override void Initialize()
         {
             base.Initialize();
+            SuccessiveDashesLeft = SuccessiveDashAmount;
         }
 
         protected override void HandleInput()
         {
             base.HandleInput();
+
+            if(Input.GetKeyDown(KeyCode.O))
+            {
+                StartDash();
+            }
+            
         }
 
         protected override void UpdateBrickProcess()
         {
             base.UpdateBrickProcess();
+            //// If the character is dashing, we cancel the gravity
+            //if (_movement.CurrentState == CharacterStates.MovementStates.Dashing)
+            //{
+            //    _controller.GravityActive(false);
+            //}
+
+            //// we reset our slope tolerance if dash didn't end naturally
+            //if ((!_dashEndedNaturally) && (_movement.CurrentState != CharacterStates.MovementStates.Dashing))
+            //{
+            //    _dashEndedNaturally = true;
+            //    _controller.Parameters.MaximumSlopeAngle = _slopeAngleSave;
+            //}
+
+            //HandleAmountOfDashesLeft();
         }
+        ///// <summary>
+        ///// Checks if conditions are met to reset the amount of dashes left
+        ///// </summary>
+        //protected virtual void HandleAmountOfDashesLeft()
+        //{
+        //    if ((SuccessiveDashesLeft >= SuccessiveDashAmount) || (Time.time - _lastDashAt < DashCooldown))
+        //    {
+        //        return;
+        //    }
+
+        //    switch (SuccessiveDashResetMethod)
+        //    {
+        //        case SuccessiveDashResetMethods.Time:
+        //            if (Time.time - _lastDashAt > SuccessiveDashResetDuration)
+        //            {
+        //                SetSuccessiveDashesLeft(SuccessiveDashAmount);
+        //            }
+        //            break;
+        //        case SuccessiveDashResetMethods.Grounded:
+        //            if (_controller.State.IsGrounded)
+        //            {
+        //                SetSuccessiveDashesLeft(SuccessiveDashAmount);
+        //            }
+        //            break;
+        //    }
+        //}
+
+        ///// <summary>
+        ///// A method to reset the amount of successive dashes left
+        ///// </summary>
+        ///// <param name="newAmount"></param>
+        //public virtual void SetSuccessiveDashesLeft(int newAmount)
+        //{
+        //    SuccessiveDashesLeft = newAmount;
+        //}
 
         private void StartDash()
         {
-            //if (dashCondition() == false)
-            //    return;
+            if (DashConditions() == false)
+                return;
+
+            startTime = Time.time;
+            dashEndedNaturally = false;
+            initialPosition = this.transform.position;
+            distanceTraveled = 0;
+            shouldKeepDashing = true;
+            cooldownTimeStamp = Time.time + DashCooldown;
+            lastDashAt = Time.time;
+
+            if (LimitedDashes)
+            {
+                SuccessiveDashesLeft -= 1;
+            }
+
+            // we prevent our character from going through slopes
+            slopeAngleSave = controller.MaximumSlopeAngle;
+            controller.MaximumSlopeAngle = 0;
+
+            ComputeDashDirection();
 
             dashCoroutine = Dash();
             StartCoroutine(dashCoroutine);
@@ -91,20 +173,44 @@ namespace ControlRoom
             }
 
             // we play our exit sound 
-            StopStartFeedbacks();
-            PlayAbilityStopFeedbacks();
+            //StopStartFeedbacks();
+            //PlayAbilityStopFeedbacks();
             // once the boost is complete, if we were dashing, we make it stop and start the dash cooldown 
-            if (_movement.CurrentState == CharacterStates.MovementStates.Dashing)
+            //if (_movement.CurrentState == CharacterStates.MovementStates.Dashing)
+            //{
+            //    if (controller.Conditions.IsGrounded)
+            //    {
+            //        _movement.ChangeState(CharacterStates.MovementStates.Idle);
+            //    }
+            //    else
+            //    {
+            //        _movement.RestorePreviousState();
+            //    }
+            //}
+        }
+
+        private void ComputeDashDirection()
+        {
+
+            dashDirection = agent.IsFacingRight ? Vector2.right : Vector2.left;
+
+        }
+
+        public bool DashConditions()
+        {
+            // if we're in cooldown between two dashes, we prevent dash
+            if (cooldownTimeStamp > Time.time)
             {
-                if (controller.Conditions.IsGrounded)
-                {
-                    _movement.ChangeState(CharacterStates.MovementStates.Idle);
-                }
-                else
-                {
-                    _movement.RestorePreviousState();
-                }
+                return false;
             }
+
+            // if we don't have dashes left, we prevent dash
+            if (SuccessiveDashesLeft <= 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
 
