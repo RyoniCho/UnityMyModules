34b57304705225c6241fc74417ebc3bb80096030
RyoniCho <carpinus0@gmail.com>
2021-08-08 15:04:18 +0900

    enemy pivot포인트 재설정

--- Assets/ControlRoomModules/Scripts/2DPlatformer/AIs/AIScripts/AIActionPatrol.cs
+++ Assets/ControlRoomModules/Scripts/2DPlatformer/AIs/AIScripts/AIActionPatrol.cs
@@ -6,24 +6,40 @@ namespace ControlRoom
 {
     public class AIActionPatrol : AIAction
     {
+        public float BoundsExtentsLeft;
+        public float BoundsExtentsRight;
 
+
+        Agent agent;
+        PhysicsController controller;
         HorizontalMovementBrick horizontalMovementBrick;
-        Vector3 startPosition;
+        Vector2 startPosition;
+        Vector2 direction;
+
+        Vector2 boundsLeft;
+        Vector2 boundsRight;
 
 
         protected override void Initialization()
         {
             base.Initialization();
 
-            this.horizontalMovementBrick = this.gameObject.GetComponentInParent<Agent>()?.FindBrick<HorizontalMovementBrick>();
+            this.agent = this.gameObject.GetComponentInParent<Agent>();
+            this.controller = this.gameObject.GetComponentInParent<PhysicsController>();
 
-            startPosition = this.transform.position;
+            this.horizontalMovementBrick = agent?.FindBrick<HorizontalMovementBrick>();
+
+            this.startPosition = this.transform.position;
+            
+            this.direction = agent.IsFacingRight? Vector2.right : Vector2.left;
+
+            EstablishBounds();
 
         }
 
         public override void StartAction()
         {
-            
+            this.Patrol();
         }
 
         public override void OnEnterState()
@@ -37,6 +53,45 @@ namespace ControlRoom
 
             this.horizontalMovementBrick.SetHorizontalMove(0f);
         }
+
+        private void Patrol()
+        {
+            CheckForDistance();
+            CheckForObstacle();
+
+            this.horizontalMovementBrick.SetHorizontalMove(direction.x);
+        }
+
+        private void CheckForObstacle()
+        {
+            if(DetectObstaclesRegularLayermask())
+                this.direction = -this.direction;
+
+        }
+
+        private bool DetectObstaclesRegularLayermask()
+        {
+            return (this.direction.x < 0 && this.controller.Conditions.IsCollidingLeft) || (this.direction.x > 0 && this.controller.Conditions.IsCollidingRight);
+        }
+
+        private void EstablishBounds()
+        {
+            this.boundsLeft = this.transform.position + Vector3.left * BoundsExtentsLeft;
+            this.boundsRight = this.transform.position + Vector3.right * BoundsExtentsRight;
+        }
+
+        private void CheckForDistance()
+        {
+            if (this.transform.position.x < this.boundsLeft.x)
+            {
+                this.direction = Vector2.right;
+            }
+            if (this.transform.position.x > this.boundsRight.x)
+            {
+                this.direction = Vector2.left;
+            }
+        }
+
     }
 }
 
