4b8b2ff5560ca02aeaca59fb284defe05363469a
RyoniCho <carpinus0@gmail.com>
2021-08-08 15:04:37 +0900

    AI action Attack 생성

---
+++ Assets/ControlRoomModules/Scripts/2DPlatformer/AIs/AIScripts/AIActionAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ControlRoom
+{
+    public class AIActionAttack : AIAction
+    {
+
+        private MeleeAttackBrick attackBrick;
+
+        protected override void Initialization()
+        {
+            base.Initialization();
+
+            
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        public override void StartAction()
+        {
+            
+        }
+
+        public override void OnEnterState()
+        {
+            base.OnEnterState();
+        }
+
+        public override void OnExitState()
+        {
+            base.OnExitState();
+        }
+
+        
+    }
+
+}
